4491af1b337b059446b7c58367fd1b19f33940e5
azizurrahman-zero <azizurrahman.zero@gmail.com>
2022-05-05 20:42:24 +0600

    inventory added

---
+++ src/components/Book/Book.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Card, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Book.css'
+
+const Book = ({ book }) => {
+    const { image, name, description, price, quantity, supplier } = book;
+    return (
+        <Col>
+            <Card className="h-100 p-4 book">
+                <img className="card-img-top" src={image} alt={name} />
+                <Card.Body className='p-0'>
+                    <Card.Title className='fs-4 fw-bold mt-4 mb-0 f-merriweather'>{name}</Card.Title>
+                    <p className='my-1'><span className='gray-color'>Supplier</span> • {supplier}</p>
+                    <p className='f-inter book-description py-3'>{description}</p>
+                </Card.Body>
+                <Card.Footer className='p-0 bg-transparent border-0'>
+                    <div className='d-flex justify-content-between fs-3 f-rasa'>
+                        <p className='m-0 gray-color'>Price: <span className='primary-color'>${price}</span></p>
+                        <p className='m-0 gray-color'>Quantity: <span className='primary-color'>{quantity}</span></p>
+                    </div>
+                    <Link to='/login'>
+                        <Button className='border-0 py-2 px-4 mt-3 rounded-3 fw-bold f-merriweather secondary-bg button'>Update</Button>
+                    </Link>
+                </Card.Footer>
+            </Card>
+        </Col>
+    );
+};
+
+export default Book;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
+import useBooks from '../../hooks/useBooks';
 import Banner from '../Banner/Banner';
+import Book from '../Book/Book';
 
 const Home = () => {
+    const [books] = useBooks();
     return (
         <Container>
             <Banner></Banner>
+            <section className='mt-5'>
+                <h3 className='fs-1 text-center fw-bold mb-4 primary-color f-merriweather'>Inventory</h3>
+                <Row xs={1} md={3} className="g-4">
+                    {
+                        books.map(book =>
+                            <Book
+                                key={book._id}
+                                book={book}
+                            ></Book>
+                        )
+                    }
+                </Row>
+            </section>
         </Container>
     );
 };
