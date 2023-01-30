05bfb5095e6f13665edd34c4f15ec9058128c688
azizurrahman-zero <azizurrahman.zero@gmail.com>
2023-01-30 16:40:37 +0600

    loader fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import useBooks from "../../hooks/useBooks";
 import Banner from "../Banner/Banner";
 import Book from "../Book/Book";
+import Loading from "../Loading/Loading";
 import ManageInventoriesButton from "../ManageInventoriesButton/ManageInventoriesButton";
 import Recommendation from "../Recommendation/Recommendation";
 import UserReviews from "../UserReviews/UserReviews";
@@ -17,11 +18,15 @@ const Home = () => {
         <h3 className="fs-1 text-center fw-bold mb-4 primary-color f-merriweather">
           Inventory
         </h3>
-        <Row xs={1} md={3} className="g-4 mb-4">
-          {books.slice(0, 6).map((book) => (
-            <Book key={book._id} book={book}></Book>
-          ))}
-        </Row>
+        {books ? (
+          <Row xs={1} md={3} className="g-4 mb-4">
+            {books.slice(0, 6).map((book) => (
+              <Book key={book._id} book={book}></Book>
+            ))}
+          </Row>
+        ) : (
+          <Loading />
+        )}
         <ManageInventoriesButton></ManageInventoriesButton>
       </section>
       <Recommendation></Recommendation>
